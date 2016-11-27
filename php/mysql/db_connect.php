6f7dcebd13bb5d31ba97105859fbf2444b33b163
frankyhung93 <frankyhung93@yahoo.com.hk>
2016-11-28 00:59:51 +0800

    added mysqli.php
    Signed-off-by: frankyhung93 <frankyhung93@yahoo.com.hk>

--- php/mysql/db_connect.php
+++ php/mysql/db_connect.php
@@ -9,3 +9,13 @@ mysql_connect($hostname, $user, $password);
 mysql_set_charset('utf8');
 @mysql_select_db($database) or die( "Unable to select database");
 mysql_query("SET NAMES 'utf8'");
+
+$mysqli = new mysqli($hostname, $user, $password, $database);
+if ($mysqli->connect_errno) {
+    echo "Failed to connect to MySQL: " . $mysqli->connect_error;
+    exit;
+}
+if (!$mysqli->set_charset("utf8")) {
+    printf("Error loading character set utf8: %s\n", $mysqli->error);
+    exit;
+}
